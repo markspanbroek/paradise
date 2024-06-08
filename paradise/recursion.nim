bf79ae77184bd39df9d2bc08a0506b4da736e8b1
Mark Spanbroek <mark@spanbroek.net>
2024-06-08 22:00:00 +0200

    do not update LL1 sets again when rule is not recursive

--- paradise/recursion.nim
+++ paradise/recursion.nim
@@ -6,21 +6,24 @@ import ./parsing
 
 func define*[Token; G: Grammar[Token]](rule: Recursion, definition: G) =
   var updating = false
+  var recursive = false
   rule.updateClosure = proc(again: var bool) =
     if not updating:
       updating = true
       definition.update()
       if not rule.canBeEmpty and definition.canBeEmpty:
         rule.canBeEmpty = definition.canBeEmpty
-        again = true
+        again = recursive
       if not (definition.first <= rule.first):
         rule.first.incl(definition.first)
-        again = true
+        again = recursive
       if not (definition.last <= rule.last):
         rule.last.incl(definition.last)
-        again = true
+        again = recursive
       rule.last.incl(rule)
       updating = false
+    else:
+      recursive = true
   rule.parseClosure = proc(automaton: Automaton[Token]) =
     bind basics.unsafeError
     proc assign() =
